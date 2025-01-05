2a8f4c2760c7eb65fe59cbb5d90a07d1baa63186
Sesili-Anthonypeter <sesilianthonypeter@gmail.com>
2025-01-05 18:11:18 +0530

    Projects updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased 
@@ -19,6 +20,7 @@ const App = () => {
       <About />
       <Technologies />
       <Experience />
+      <Projects />
     </div>
   </div>
     
